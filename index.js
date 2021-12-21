7c28a0114a053c078f4e689aefd1634d6ae01b6e
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-12-21 10:49:30 +0100

    Create javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+const countdownDisplay = document.getElementById("countdown-display")
+const countDown = document.querySelector(".countdown");
+const countHour = document.querySelector(".counthour");
+const countMinute = document.querySelector(".countminute");
+const countSecond = document.querySelector(".countsecond");
