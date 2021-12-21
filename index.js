5db22e8d81c458cc64f20ffc18b08dc0a673faf6
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-12-21 10:50:24 +0100

    Create javascript function

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const countDown = document.querySelector(".countdown");
 const countHour = document.querySelector(".counthour");
 const countMinute = document.querySelector(".countminute");
 const countSecond = document.querySelector(".countsecond");
+
+function renderCurrentDay(){
+  const christmas = 25
+  
+  const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+  let todaysDate = new Date();
+  let day = weekDays[todaysDate.getDay()];
+  countdownDisplay.innerHTML += day;
+  // Task:
+  // - Get today's date (you only need the day).
+  // - Calculate remaining days.
+  // - Display remaining days in countdownDisplay.
+}
